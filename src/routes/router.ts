3bf5f2e7552a9e1229272b11b23dc2da5fd9b7b2
ripOzziel <ozielinhov@gmail.com>
2025-05-08 23:50:10 -0700

    Implementacion de middleware de auth para rutas protegidas

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,18 +5,34 @@ import {
         markDeviceAsLost, 
         removeDeviceFromUser
     } from "../controllers/device.controller.js";
+    
 import { createUser, getUser, signOut } from "../controllers/user.controller.js";
 import { getCurrentLocation } from "../controllers/location.controller.js";
+import { authMiddleware } from '../middlewares/auth.middleware.js';
+
 
 const router = Router();
 
+// Rutas públicas
 router.post('/', createUser);
 router.post('/login', getUser);
+
+// Rutas protegidas
+router.use((req, res, next) => {
+    authMiddleware(req, res, next).catch(error => {
+        return res.status(500).json({ 
+            error: 'Error en la autenticación',
+            details: error instanceof Error ? error.message : 'Unknown error'
+        });
+    });
+});
+
+// Rutas que requieren autenticación
 router.post('/signOut', signOut);
 router.post('/assign', assignDeviceToUser);
-router.patch('/:deviceId/lost', markDeviceAsLost); // Ruta para marcar un dispositivo como perdido
-router.get('/user/:userId', getUserDevices); // Ruta para obtener los dispositivos asociados a un usuario
-router.delete('/:deviceId/delete', removeDeviceFromUser)
+router.patch('/:deviceId/lost', markDeviceAsLost);
+router.get('/user/:userId', getUserDevices);
+router.delete('/:deviceId/delete', removeDeviceFromUser);
 router.get('/:deviceId/location', getCurrentLocation);
 
 export default router;
