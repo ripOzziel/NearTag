17b9e1d2ae10e620dcdf1bb1f7dc6960489fb186
ripOzziel <ozielinhov@gmail.com>
2025-05-08 21:34:30 -0700

    endpoint signOut

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -93,3 +93,40 @@ export const getUser = async (req: Request, res: Response): Promise<any> => {
     })
   }
 }
+
+export const signOut = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({
+        error: 'Token no proporcionado'
+      });
+    }
+
+    const token = authHeader.split(' ')[1];
+    
+    try {
+      const decoded = jwt.verify(token, SECRET_KEY) as { id: string, email: string };
+      
+      await prisma.user.update({
+        where: { id: decoded.id },
+        data: {
+          jwt_hash: "" // mejor que null??
+        }
+      });
+      
+      return res.status(200).json({
+        message: 'Sesión cerrada exitosamente'
+      });
+    } catch (jwtError) {
+      return res.status(401).json({
+        error: 'Token inválido o expirado'
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Error al cerrar sesión',
+      detail: err
+    });
+  }
+};
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,13 +5,14 @@ import {
         markDeviceAsLost, 
         removeDeviceFromUser
     } from "../controllers/device.controller.js";
-import { createUser, getUser } from "../controllers/user.controller.js";
+import { createUser, getUser, signOut } from "../controllers/user.controller.js";
 import { getCurrentLocation } from "../controllers/location.controller.js";
 
 const router = Router();
 
 router.post('/', createUser);
 router.post('/login', getUser);
+router.post('/signOut', signOut);
 router.post('/assign', assignDeviceToUser);
 router.patch('/:deviceId/lost', markDeviceAsLost); // Ruta para marcar un dispositivo como perdido
 router.get('/user/:userId', getUserDevices); // Ruta para obtener los dispositivos asociados a un usuario
